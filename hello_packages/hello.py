9e13a3a611f44f9331fb973235d030e9c11092be
Petr Cvengros <petr@cvengros.cz>
2022-10-26 11:54:58 +0200

    script params

--- hello_packages/hello.py
+++ hello_packages/hello.py
@@ -1,7 +1,8 @@
 """Hello world"""
+import sys
 
 def hello_world(name: str):
     print(f"hello {name}")
 
 if __name__ == "__main__":
-    hello_world('from script')
+    hello_world(f'from script, my params: {sys.argv}')
